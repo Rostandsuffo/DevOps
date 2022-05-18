0ca41393ca021a86a61b15adbb9153f252f1980a
Rostandsuffo <98893795+Rostandsuffo@users.noreply.github.com>
2022-05-18 23:01:46 +0200

    Create window.py

---
+++ window.py
@@ -0,0 +1,9 @@
+from tkinter import *
+#creer une premiere fenetre
+window =Tk()
+window.title ("My application")
+window.geometry("300*300)
+                
+window.mainloop()
+                
+                
